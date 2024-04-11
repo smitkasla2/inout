db9b35de4af2375f8ceb6f707c0211b03d01c476
smitkasla2 <96909363+smitkasla2@users.noreply.github.com>
2024-04-11 19:35:05 -0400

    a

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
 import streamlit as st
-from PIL import Image
+st.title("Image Size Checker")
 
-def main():
-    st.title("Image Size Checker")
-
-    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        width, height = image.size
-        st.write(f"Image size: {width}x{height}")
-
-if __name__ == "__main__":
-    main()
